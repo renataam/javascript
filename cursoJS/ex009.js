8f651965d5e780f977b9538bc2da6ead0d2191cd
Renata Monteiro <108954513+renataam@users.noreply.github.com>
2022-07-20 16:49:35 -0300

    update

---
+++ cursoJS/ex009.js
@@ -0,0 +1,7 @@
+var vel = 50
+console.log(`a velocidade do seu carro é ${vel}km/h.`)
+if(vel>60){
+    console.log('você altrapassou o limite de velocidade e será multado.')
+}
+console.log('dirija sempre com cinto de segurança.')
+//condição simples
