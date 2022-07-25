9562afb52f3558279670e40d05633a4cad4b05e9
Renata Monteiro <108954513+renataam@users.noreply.github.com>
2022-07-25 20:48:16 -0300

    up

---
+++ cursoJS/12.js
@@ -0,0 +1,9 @@
+var idade =22
+if(idade < 16) {
+    console.log('Não vota')
+} else if (idade >= 16 && idade <18) {
+        console.log('Voto opcional')
+    } else ('idade >=18') {
+        console.log('voto obrigatório')
+}
+ 
