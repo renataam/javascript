363a8fe6ce72f590dc800ff46a244373407be447
Renata Monteiro <108954513+renataam@users.noreply.github.com>
2022-07-26 18:08:21 -0300

    estudos

--- exercícios/ex-bom-dia-noite/script.js
+++ exercícios/ex-bom-dia-noite/script.js
@@ -1,1 +1,18 @@
-window.alert("Olá") 
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'manha.png'
+        document.body.style.background = '#F2D4B8'
+    } else if (hora >= 12 && hora <= 17) {
+        img.src = 'tarde.png'
+        document.body.style.background = '#B44216'
+    } else {
+        img.src = 'noite.png'
+        document.body.style.background = '#463F35'
+    }
+}
+
