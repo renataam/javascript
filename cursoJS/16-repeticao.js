2e6016b89e2125077f26bb605f2d678a841f8ee4
Renata Monteiro <108954513+renataam@users.noreply.github.com>
2022-07-28 17:27:37 -0300

    estudos

---
+++ cursoJS/16-repeticao.js
@@ -0,0 +1,22 @@
+/* REPETIÇÃO "MANUAL"
+console.log('oi!')
+console.log('oi!')
+console.log('oi!')
+console.log('oi!')
+console.log('oi!')
+console.log('oi!')*/
+
+/* REPETIÇÃO COM TESTE LÓGICO NO INÍCIO
+var c = 1
+while (c <= 6) {
+    console.log('oi!')
+    c++ //significa C<1
+}
+*/
+
+//REPETIÇÃO COM TESTE LÓGICO AO FINAL
+var c = 1
+do {
+    console.log('oi!')
+    c++ //significa C<1
+} while (c <= 6)
